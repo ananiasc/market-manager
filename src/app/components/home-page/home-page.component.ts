7328f299e4f42db77a724e575b2e26d4091be1ac
Ananias Caetano <ananiascaetano556@gmail.com>
2025-03-08 18:51:59 -0300

    update angular

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './home-page.component.html'
+    selector: 'app-home-page',
+    imports: [],
+    templateUrl: './home-page.component.html'
 })
 export class HomePageComponent {
 
