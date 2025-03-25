8dd988d4c1caacf06ce59701f5593c7997cdc017
Ananias Caetano <ananiascaetano556@gmail.com>
2025-03-24 22:31:22 -0300

    feat: defining menu and user avatar image

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { ImagesUrl } from '../../utils/constants';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,12 +11,32 @@ import { Component } from '@angular/core';
 export class DashboardComponent {
   isCadastroOpen = false;
   isMarketOpen = false;
+  img = {
+    setting: ImagesUrl.SETTINGS,
+    users: ImagesUrl.USERS,
+    products: ImagesUrl.PRODUCTS,
+    clients: ImagesUrl.CLIENTS,
+    sales: ImagesUrl.SALES,
+    catalog: ImagesUrl.CATALOG,
+    closed: ImagesUrl.MENU_CLOSED,
+    expanded: ImagesUrl.MENU_EXPANDED,
+    toggleCatalog: ImagesUrl.MENU_CLOSED,
+    brands: ImagesUrl.BRANDS,
+    categories: ImagesUrl.CATEGORIES,
+    userAvatar: ImagesUrl.USER_AVATAR
+  }
 
   toggleMenu(menu: string): void {
-    if (menu === 'cadastro') {
+    if (menu === 'catalogo') {
       this.isCadastroOpen = !this.isCadastroOpen;
-    } else if (menu === 'market') {
-      this.isMarketOpen = !this.isMarketOpen;
+      this.img.toggleCatalog = this.toggleMenuImg(this.isCadastroOpen);
     }
+  }
+
+  private toggleMenuImg (isOpen: boolean): string {
+    if (isOpen) {
+      return this.img.expanded;
+    }
+    return this.img.closed;
   }
 }
