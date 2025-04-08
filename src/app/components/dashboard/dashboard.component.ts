2d3108d51826e177c4e17f732efd1e8e1be4b0c2
Ananias Caetano <ananiascaetano556@gmail.com>
2025-04-07 21:33:06 -0300

    remove: removing unused code

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -8,36 +8,5 @@ import { ImagesUrl } from '../../utils/constants';
   templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent {
-  isCatalogOpen = false;
-  isMarketOpen = false;
-
-  img = {
-    settings: ImagesUrl.SETTINGS,
-    users: ImagesUrl.USERS,
-    products: ImagesUrl.PRODUCTS,
-    clients: ImagesUrl.CLIENTS,
-    sales: ImagesUrl.SALES,
-    catalog: ImagesUrl.CATALOG,
-    closed: ImagesUrl.MENU_CLOSED,
-    expanded: ImagesUrl.MENU_EXPANDED,
-    toggleCatalog: ImagesUrl.MENU_CLOSED,
-    brands: ImagesUrl.BRANDS,
-    categories: ImagesUrl.CATEGORIES,
-    userAvatar: ImagesUrl.USER_AVATAR,
-    promotions: ImagesUrl.PROMOTIONS,
-  }
-
-  toggleMenu(menu: string): void {
-    if (menu === 'catalogo') {
-      this.isCatalogOpen = !this.isCatalogOpen;
-      this.img.toggleCatalog = this.toggleMenuImg(this.isCatalogOpen);
-    }
-  }
-
-  private toggleMenuImg (isOpen: boolean): string {
-    if (isOpen) {
-      return this.img.expanded;
-    }
-    return this.img.closed;
-  }
+  
 }
